9e25187bbca4dd06b775b56d453dd9ade40f4933
Stefano Bergamini Poletto <sbpoletto@hotmail.com>
2023-06-13 18:29:58 -0300

    Created the footer and added the mobile style progress tech, mobile css needed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import linkLogo from '../../Assets/imgs/linkedin.png'
+import gitLogo from '../../Assets/imgs/git-logo.png'
+import email from '../../Assets/imgs/email.png'
+import telefone from '../../Assets/imgs/telefone.png'
+
 import './Footer.scss'
 
 export default function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <footer>
+      <div class="footer">
+        <div class="button" href="/">
+          <img src={email} alt="Button 1" />
+            <span>Email</span>
+            <span>stefano.bpoletto@gmail.com</span>
+        </div>
+        <div class="button" href="/">
+          <img src={telefone} alt="Button 2" />
+            <span>Telefone</span>
+            <span>+55 (48) 99694-4035</span>
+        </div>
+        <a class="button" href="https://www.linkedin.com/in/stefanobp/" target="_blank" rel="noreferrer">
+          <img src={linkLogo} alt="Button 3" />
+            <span>LinkedIn</span>
+            <span>@stefanobp</span>
+        </a>
+        <a class="button" href="https://github.com/stefanobergamini/Sbp" target="_blank" rel="noreferrer">
+          <img src={gitLogo} alt="Button 3" />
+            <span>GitHub</span>
+            <span>@stefanobergamini</span>
+        </a>
+      </div>
+    </footer>
   )
 }
--- src/Components/Sobre/Progress.js
+++ src/Components/Sobre/Progress.js
@@ -5,77 +5,153 @@ import { textVariant } from '../../Utils/motion';
 
 export default function Progress() {
     return (
-        <motion.div
-            initial="hidden"
-            whileInView="show">
-            <motion.div variants={textVariant()}
-                className="container-progressbars">
-                <div className='container-progressbars-completed'>
+        <>
+            <motion.div
+                className='desktop'
+                initial="hidden"
+                whileInView="show">
+                <motion.div variants={textVariant()}
+                    className="container-progressbars">
+                    <div className='container-progressbars-completed'>
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-react shadow-react"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-react">React.js</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-html shadow-html"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-html">HTML</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-css shadow-css"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-css">CSS</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-scss shadow-scss"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-scss">SCSS</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-js shadow-js"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-js">JavaScript</span>
+                        </div>
+                    </div>
                     <div className="progressbar">
                         <svg className="progressbar-svg">
-                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-react shadow-react"> </circle>
+                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-vue shadow-vue"> </circle>
                         </svg>
-                        <span className="progressbar-text shadow-react">React.js</span>
+                        <span className="progressbar-text shadow-vue">Vue.js</span>
                     </div>
+                    <div className="progressbar">
+                        <svg className="progressbar-svg">
+                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-python shadow-python"> </circle>
+                        </svg>
+                        <span className="progressbar-text shadow-python">Python</span>
+                    </div>
+                    <div className="progressbar">
+                        <svg className="progressbar-svg">
+                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-angular shadow-angular"> </circle>
+                        </svg>
+                        <span className="progressbar-text shadow-angular">Angular.js</span>
+                    </div>
+
 
                     <div className="progressbar">
                         <svg className="progressbar-svg">
-                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-html shadow-html"> </circle>
+                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-node shadow-node"> </circle>
                         </svg>
-                        <span className="progressbar-text shadow-html">HTML</span>
+                        <span className="progressbar-text shadow-node">Node.js</span>
                     </div>
 
-                    <div className="progressbar">
-                        <svg className="progressbar-svg">
-                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-css shadow-css"> </circle>
-                        </svg>
-                        <span className="progressbar-text shadow-css">CSS</span>
-                    </div>
-
-                    <div className="progressbar">
-                        <svg className="progressbar-svg">
-                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-scss shadow-scss"> </circle>
-                        </svg>
-                        <span className="progressbar-text shadow-scss">SCSS</span>
-                    </div>
-
-                    <div className="progressbar">
-                        <svg className="progressbar-svg">
-                            <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-js shadow-js"> </circle>
-                        </svg>
-                        <span className="progressbar-text shadow-js">JavaScript</span>
-                    </div>
-                </div>
-                <div className="progressbar">
-                    <svg className="progressbar-svg">
-                        <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-vue shadow-vue"> </circle>
-                    </svg>
-                    <span className="progressbar-text shadow-vue">Vue.js</span>
-                </div>
-                <div className="progressbar">
-                    <svg className="progressbar-svg">
-                        <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-python shadow-python"> </circle>
-                    </svg>
-                    <span className="progressbar-text shadow-python">Python</span>
-                </div>
-                <div className="progressbar">
-                    <svg className="progressbar-svg">
-                        <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-angular shadow-angular"> </circle>
-                    </svg>
-                    <span className="progressbar-text shadow-angular">Angular.js</span>
-                </div>
 
 
-                <div className="progressbar">
-                    <svg className="progressbar-svg">
-                        <circle cx="80" cy="80" r="70" className="progressbar-svg-circle circle-node shadow-node"> </circle>
-                    </svg>
-                    <span className="progressbar-text shadow-node">Node.js</span>
-                </div>
-
-
-
+                </motion.div>
             </motion.div>
-        </motion.div>
+            <motion.div
+                className='mobile'
+                initial="hidden"
+                whileInView="show">
+                <motion.div variants={textVariant()}
+                    className="container-progressbars">
+                    <div className='container-progressbars-completed'>
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-react shadow-react"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-react">React.js</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-html shadow-html"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-html">HTML</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-css shadow-css"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-css">CSS</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-scss shadow-scss"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-scss">SCSS</span>
+                        </div>
+
+                        <div className="progressbar">
+                            <svg className="progressbar-svg">
+                                <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-js shadow-js"> </circle>
+                            </svg>
+                            <span className="progressbar-text shadow-js">JavaScript</span>
+                        </div>
+                    </div>
+                    <div className="progressbar">
+                        <svg className="progressbar-svg">
+                            <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-vue shadow-vue"> </circle>
+                        </svg>
+                        <span className="progressbar-text shadow-vue">Vue.js</span>
+                    </div>
+                    <div className="progressbar">
+                        <svg className="progressbar-svg">
+                            <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-python shadow-python"> </circle>
+                        </svg>
+                        <span className="progressbar-text shadow-python">Python</span>
+                    </div>
+                    <div className="progressbar">
+                        <svg className="progressbar-svg">
+                            <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-angular shadow-angular"> </circle>
+                        </svg>
+                        <span className="progressbar-text shadow-angular">Angular.js</span>
+                    </div>
+
+
+                    <div className="progressbar">
+                        <svg className="progressbar-svg">
+                            <circle cx="55" cy="55" r="50" className="progressbar-svg-circle circle-node shadow-node"> </circle>
+                        </svg>
+                        <span className="progressbar-text shadow-node">Node.js</span>
+                    </div>
+
+
+
+                </motion.div>
+            </motion.div>
+        </>
     )
 }
